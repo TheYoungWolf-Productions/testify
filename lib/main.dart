8da77fb1dd4c90611270d3d3a6186893416dead9
TheYoungWolf-Productions <imafraz@hotmail.com>
2022-01-26 21:14:32 +0500

    Convert to Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:testify/screens/wrapper.dart';
+import 'package:testify/auth_storage.dart';
+import 'package:testify/screens/Authentication/login.dart';
+import 'package:testify/screens/home.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
+
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Wrapper(),
-    );
+        home: Login(
+      fromWhere: "main",
+    ));
   }
 }
